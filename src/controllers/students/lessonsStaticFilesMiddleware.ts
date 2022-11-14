d54007ae306619f6ddae3259da5fcb3c43fd7118
Dave Welsh <dave@qccareerschool.com>
2022-11-14 09:38:40 -0500

    Remove COEP header from lessons

--- src/controllers/students/lessonsStaticFilesMiddleware.ts
+++ src/controllers/students/lessonsStaticFilesMiddleware.ts
@@ -12,6 +12,7 @@ export class LessonsStaticFilesMiddleware extends BaseMiddleware<void, void> {
 
   // eslint-disable-next-line @typescript-eslint/require-await
   protected async executeImpl(): Promise<void> {
+    this.res.removeHeader('Cross-Origin-Embedder-Policy');
     express.static(LessonsStaticFilesMiddleware.path)(this.req, this.res, this.next);
   }
 }
