97ee8dda7bbeb0f7413f7769f114331bc7ef49e8
Dave Welsh <dave@qccareerschool.com>
2023-04-06 07:20:20 -0400

    Allow HEAD requests

--- src/controllers/baseController.ts
+++ src/controllers/baseController.ts
@@ -100,7 +100,7 @@ export abstract class BaseController<RequestDTO = unknown, ResponseDTO = unknown
 
   // Helper functions
   protected isGetMethod(): boolean {
-    return this.req.method === 'GET';
+    return this.req.method === 'GET' || this.req.method === 'HEAD';
   }
 
   protected isPostMethod(): boolean {
