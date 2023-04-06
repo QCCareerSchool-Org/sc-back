4aab0411f7d2afe7f3b0c0d72fbd61bec60de3cb
Dave Welsh <dave@qccareerschool.com>
2023-04-06 07:06:01 -0400

    Move unauthenticated routes to unauthenticated router

--- src/frameworks/express/router.ts
+++ src/frameworks/express/router.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 
+import { DownloadNewAssignmentMediumController } from '../../controllers/administrators/downloadNewAssignmentMediumController.js';
+import { DownloadNewPartMediumController } from '../../controllers/administrators/downloadNewPartMediumController.js';
 import { DownloadCourseHeaderImageController } from '../../controllers/downloadCourseHeaderImageController.js';
 import { downloadCourseIconImageController } from '../../controllers/downloadCourseIconImageController.js';
 import type { Route } from './applyRoutes.js';
@@ -10,6 +12,9 @@ export const router = Router();
 const routes: Route[] = [
   [ 'get', '/courseHeaderImages/:courseId', DownloadCourseHeaderImageController ],
   [ 'get', '/courseIconImages/:courseId', downloadCourseIconImageController ],
+
+  [ 'get', '/:administratorId/newAssignmentMedia/:mediumId/file', DownloadNewAssignmentMediumController ],
+  [ 'get', '/:administratorId/newPartMedia/:mediumId/file', DownloadNewPartMediumController ],
 ];
 
 applyRoutes(router, routes);
