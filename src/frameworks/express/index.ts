ac732aa419eb96c7706488cad2e3d50840987133
Dave Welsh <dave@qccareerschool.com>
2023-09-14 13:30:37 -0400

    Added Accept-Ranges header to allowed headers

--- src/frameworks/express/index.ts
+++ src/frameworks/express/index.ts
@@ -22,7 +22,7 @@ const { port } = environmentConfigService.config;
 const corsOptions: CorsOptions = {
   origin: process.env.NODE_ENV === 'production' ? 'https://studentcenter.qccareerschool.com' : 'http://localhost:3000',
   credentials: true,
-  exposedHeaders: [ 'Content-Disposition' ],
+  exposedHeaders: [ 'Content-Disposition', 'Accept-Ranges' ],
 };
 
 const app = express();
