5bbcf44d4f7ec2effc23691c3ae63b70a093c02d
Dave Welsh <dave@qccareerschool.com>
2023-09-14 13:52:42 -0400

    Pass range header information to interactor

--- src/controllers/students/downloadNewSubmissionFeedbackController.ts
+++ src/controllers/students/downloadNewSubmissionFeedbackController.ts
@@ -3,9 +3,13 @@ import * as yup from 'yup';
 import type { DownloadNewSubmissionFeedbackResponseDTO } from '../../interactors/students/downloadNewSubmissionFeedbackInteractor.js';
 import { DownloadNewSubmissionFeedbackFileNotFound, DownloadNewSubmissionFeedbackFileReadError, DownloadNewSubmissionFeedbackNotClosed, DownloadNewSubmissionFeedbackNotFound, DownloadNewSubmissionFeedbackNotSubmitted, DownloadNewSubmissionFeedbackSkipped } from '../../interactors/students/downloadNewSubmissionFeedbackInteractor.js';
 import { downloadNewSubmissionFeedbackInteractor } from '../../interactors/students/index.js';
+import type { ByteRange } from '../baseController.js';
 import { BaseController } from '../baseController.js';
 
 type Request = {
+  headers: {
+    range?: string;
+  };
   params: {
     /** numeric string */
     studentId: string;
@@ -21,14 +25,20 @@ type Response = DownloadNewSubmissionFeedbackResponseDTO;
 export class DownloadNewSubmissionFeedbackController extends BaseController<Request, Response> {
 
   protected async validate(): Promise<Request | false> {
+    const headersSchema: yup.SchemaOf<Request['headers']> = yup.object({
+      range: yup.string(),
+    });
     const paramsSchema: yup.SchemaOf<Request['params']> = yup.object({
       studentId: yup.string().matches(/^\d+$/u).defined(),
       courseId: yup.string().matches(/^\d+$/u).defined(),
       submissionId: yup.string().matches(/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/iu).defined(),
     });
     try {
-      const params = await paramsSchema.validate(this.req.params);
-      return { params };
+      const [ headers, params ] = await Promise.all([
+        headersSchema.validate(this.req.headers),
+        paramsSchema.validate(this.req.params),
+      ]);
+      return { headers, params };
     } catch (error) {
       if (error instanceof Error) {
         this.badRequest(error.message);
@@ -39,16 +49,30 @@ export class DownloadNewSubmissionFeedbackController extends BaseController<Requ
     }
   }
 
-  protected async executeImpl({ params }: Request): Promise<void> {
+  protected async executeImpl({ headers, params }: Request): Promise<void> {
     if (!this.isGetMethod()) {
       return this.methodNotAllowed();
+    }
+
+    let byteRange: ByteRange | false | undefined;
+    if (headers.range?.startsWith('bytes=')) {
+      byteRange = this.getByteRange(headers.range);
+    }
+    if (byteRange === false) {
+      return this.rangeNotSatisfiable();
     }
 
     const studentId = parseInt(params.studentId, 10);
     const courseId = parseInt(params.courseId, 10);
     const { submissionId } = params;
 
-    const result = await downloadNewSubmissionFeedbackInteractor.execute({ studentId, courseId, submissionId });
+    const result = await downloadNewSubmissionFeedbackInteractor.execute({
+      studentId,
+      courseId,
+      submissionId,
+      startByte: byteRange?.start,
+      endByte: byteRange?.end,
+    });
 
     if (result.success) {
       return this.sendInteractorFileStream(result.value);
