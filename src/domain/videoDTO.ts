e9bc4e5a80f30f5baed7e2564915f0c44ef6e641
Dave Welsh <dave@qccareerschool.com>
2024-07-12 08:53:36 -0400

    Allow for null posterSrc

--- src/domain/videoDTO.ts
+++ src/domain/videoDTO.ts
@@ -2,7 +2,7 @@ export type VideoDTO = {
   /** uuid */
   videoId: string;
   src: string;
-  posterSrc: string;
+  posterSrc: string | null;
   captionSrc: string | null;
   title: string;
   description: string | null;
