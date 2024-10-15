219aa02d29d5f21249a927b25cdf0e59d5d3dfab
Dave Welsh <dave@qccareerschool.com>
2024-10-15 11:46:56 -0400

    Fixed OR clause

--- src/interactors/tutors/downloadNewAssignmentMediumInteractor.ts
+++ src/interactors/tutors/downloadNewAssignmentMediumInteractor.ts
@@ -44,7 +44,7 @@ export class DownloadNewAssignmentMediumInteractor implements IInteractor<Downlo
           assignmentMediumId: assignmentMediumIdBin,
           newAssignments: { some: { newAssignment: { newSubmission: {
             NOT: { submitted: null },
-            OR: { enrollment: { tutorId }, tutorId },
+            OR: [ { enrollment: { tutorId } }, { tutorId } ],
           } } } },
         },
       });
