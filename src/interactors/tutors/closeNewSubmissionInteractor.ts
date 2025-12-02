3a91be6ea78e7803030cb9b0161ce9479322aa9b
Dave Welsh <dave@qccareerschool.com>
2025-12-02 15:00:40 -0500

    Added bcc

--- src/interactors/tutors/closeNewSubmissionInteractor.ts
+++ src/interactors/tutors/closeNewSubmissionInteractor.ts
@@ -379,33 +379,36 @@ export class CloseNewSubmissionInteractor implements IInteractor<CloseNewSubmiss
   private async sendDGKitShippingEmail(submission: NewSubmission & { enrollment: Enrollment & { student: Student; course: Course } }): Promise<void> {
     const name = 'Shipping Department';
     const to = 'shipping@qccareerschool.com';
+    const bcc = 'dave@qccareerschool.com';
     const subject = `${submission.enrollment.course.code}${submission.enrollment.studentNumber} Submission ${submission.unitLetter} Has Been Marked`;
     const textBody = `${submission.enrollment.student.firstName} ${submission.enrollment.student.lastName} (${submission.enrollment.course.code}${submission.enrollment.studentNumber})'s Submission ${submission.unitLetter} has been marked. Please ship clippers and combs if they haven't already been sent (check student notes).`;
     const htmlBody = `<p>${textBody}</p>`;
 
-    await this.emailService.send(name, to, subject, htmlBody, textBody);
+    await this.emailService.send(name, to, subject, htmlBody, textBody, undefined, { bcc });
   }
 
   private async sendMakeupKitShippingEmail(submission: NewSubmission & { enrollment: Enrollment & { student: Student; course: Course } }): Promise<void> {
     const name = 'Shipping Department';
     const to = 'shipping@qccareerschool.com';
+    const bcc = 'dave@qccareerschool.com';
     const body = [ `${submission.enrollment.student.firstName} ${submission.enrollment.student.lastName} (${submission.enrollment.course.code}${submission.enrollment.studentNumber})'s Submission ${submission.unitLetter} has been marked. Please ship all applicable kits for _any_ makeup courses that haven't already been shipped.`, 'Please check the student file to verify which kits the student should receive.' ];
     const subject = `${submission.enrollment.course.code}${submission.enrollment.studentNumber} Submission ${submission.unitLetter} Has Been Marked`;
     const textBody = body.join('\n\n');
     const htmlBody = body.map(b => `<p>${b}</p>`).join('\n');
 
-    await this.emailService.send(name, to, subject, htmlBody, textBody);
+    await this.emailService.send(name, to, subject, htmlBody, textBody, undefined, { bcc });
   }
 
   private async sendExternshipEmail(submission: NewSubmission & { enrollment: Enrollment & { student: Student; course: Course } }): Promise<void> {
     const name = 'Externship Admin';
     const to = 'info@qccareerschool.com';
+    const bcc = 'dave@qccareerschool.com';
     const body = [ `${submission.enrollment.student.firstName} ${submission.enrollment.student.lastName} (${submission.enrollment.course.code}${submission.enrollment.studentNumber})'s Submission ${submission.unitLetter} has been marked.` ];
     const subject = `[Externship] ${submission.enrollment.course.code}${submission.enrollment.studentNumber} Submission ${submission.unitLetter} Has Been Marked`;
     const textBody = body.join('\n\n');
     const htmlBody = body.map(b => `<p>${b}</p>`).join('\n');
 
-    await this.emailService.send(name, to, subject, htmlBody, textBody);
+    await this.emailService.send(name, to, subject, htmlBody, textBody, undefined, { bcc });
   }
 
   private async sendStudentEmail(fullName: string, to: string, schoolName: string, courseName: string, unitLetter: string, grade: string, failed: boolean): Promise<void> {
