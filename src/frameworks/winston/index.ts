9c135794c05680e4cc51c7de1476f8e0299c559b
Dave Welsh <dave@qccareerschool.com>
2026-04-22 14:25:34 -0400

    Added json formatting

--- src/frameworks/winston/index.ts
+++ src/frameworks/winston/index.ts
@@ -1,11 +1,37 @@
 import { createLogger, format, transports } from 'winston';
 
+/**
+ * If the data passed to the logger is an instance of Error, transform the stack trace into an array
+ * @param _key
+ * @param value
+ */
+const replacer = (_key: string, value: unknown): unknown => {
+  if (value instanceof Error) {
+    return Object.getOwnPropertyNames(value).reduce((previousValue, currentValue) => {
+      if (currentValue === 'stack') {
+        return {
+          ...previousValue,
+          stack: value.stack?.split('\n').map(v => {
+            v = v.trim();
+            return v.startsWith('at ') ? v.slice(3) : v;
+          }),
+        };
+      }
+      return {
+        ...previousValue,
+        [currentValue]: value[currentValue as keyof Error],
+      };
+    }, {});
+  }
+  return value;
+};
+
 export const winston = createLogger({
   level: 'info',
   // format: format.json(),
   format: format.combine(
     format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-    format.json(),
+    format.json({ space: 2, replacer }),
   ),
   // defaultMeta: { service: 'user-service' },
   transports: [
